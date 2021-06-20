a87955a690a7508903204317caa70e6c08707476
sagspot <ollysag@gmail.com>
2021-06-20 16:02:37 +0300

    add auth

--- components/orders/orders.js
+++ components/orders/orders.js
@@ -4,10 +4,10 @@ const checkAuth = require('../middlewares/auth');
 
 // Add auth to all
 
-router.get('/', ordersController.orders_get_all);
-router.post('/', ordersController.orders_post_create);
-router.get('/:orderId', ordersController.orders_get_order);
-router.patch('/:orderId', ordersController.orders_patch_order);
-router.delete('/:orderId', ordersController.orders_delete);
+router.get('/', checkAuth, ordersController.orders_get_all);
+router.post('/', checkAuth, ordersController.orders_post_create);
+router.get('/:orderId', checkAuth, ordersController.orders_get_order);
+router.patch('/:orderId', checkAuth, ordersController.orders_patch_order);
+router.delete('/:orderId', checkAuth, ordersController.orders_delete);
 
 module.exports = router;
--- components/products/products.js
+++ components/products/products.js
@@ -3,9 +3,17 @@ const productsController = require('./productController');
 const checkAuth = require('../middlewares/auth');
 
 router.get('/', productsController.products_get_all);
-router.post('/', productsController.products_post_create); // add checkAuth
+router.post('/', checkAuth, productsController.products_post_create); // add checkAuth
 router.get('/:productId', productsController.products_get_product);
-router.patch('/:productId', productsController.products_patch_product); // add checkAuth
-router.delete('/:productId', productsController.products_delete_product); // add checkAuth
+router.patch(
+  '/:productId',
+  checkAuth,
+  productsController.products_patch_product
+);
+router.delete(
+  '/:productId',
+  checkAuth,
+  productsController.products_delete_product
+);
 
 module.exports = router;
